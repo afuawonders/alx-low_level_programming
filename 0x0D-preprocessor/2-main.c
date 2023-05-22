eea3e70ecb2b1e1071eb5a21244e98b7261a6e76
afuawonders <afuawonders@gmail.com>
2023-05-22 20:10:54 +0000

    c preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints file name compiled from
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("File compiled from: %s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+This project talks about preprocessors. What macros are and how to use them
+What the most common predefined macros are and how to include guard your header files.
