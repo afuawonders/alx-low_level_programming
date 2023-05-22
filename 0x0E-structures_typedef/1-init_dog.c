c8a8fd17d15df8f8f61939d578e6567ae21c5027
afuawonders <afuawonders@gmail.com>
2023-05-22 21:09:35 +0000

    c structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+* init_dog - initializes a variable of type struct dog
+* @d: pointer to struct dog to initialize
+* @name: Name to initialize
+* @age: Age to initialize
+* @owner: Owner to initialize
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+return;
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Contains details on C - Structures, typedef. Here you learn What structures are,
+when, why and how to use them and then also how to use typedef.
