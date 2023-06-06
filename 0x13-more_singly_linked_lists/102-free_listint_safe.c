02e9ba71322ee7b29b76064f8de515d3dd8d0d81
afuawonders <afuawonders@gmail.com>
2023-06-06 10:16:44 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+* free_listint_safe - frees a linked list
+* @h: pointer to the first node in the linked list
+* Return: number of elements in the freed list
+*/
+size_t free_listint_safe(listint_t **h)
+{
+size_t lenalx = 0;
+int diff;
+listint_t *temp;
+if (!h || !*h)
+return (0);
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+lenalx++;
+}
+else
+{
+free(*h);
+*h = NULL;
+lenalx++;
+break;
+}
+}
+*h = NULL;
+return (lenalx);
+}
