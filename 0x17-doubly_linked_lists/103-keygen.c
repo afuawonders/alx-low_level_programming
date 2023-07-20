1b85be96e29346ceb1c5fd52a1a85c8813ef3445
afuawonders <afuawonders@gmail.com>
2023-07-20 23:43:03 +0000

    doubly linked list

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+* add_dnodeint_end - adds a new node at the end of a dlistint_t list
+* @head: head of the list
+* @n: value of the element
+* Return: the address of the new element or NULL if it fails
+*/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new;
+dlistint_t *last = *head;
+
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+
+new->n = n;
+new->next = NULL;
+
+if (*head == NULL)
+{
+new->prev = NULL;
+*head = new;
+return (new);
+}
+
+while (last->next != NULL)
+last = last->next;
+
+last->next = new;
+new->prev = last;
+
+return (new);
+}
